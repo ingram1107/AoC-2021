fa46f7578b1ec2f57cb25a57ee98d492ede84158
Little Clover <littleclover909@gmail.com>
2021-12-13 21:20:09 +0800

    Sovled Day 13's puzzles

---
+++ day13-puzzle1.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+static bool isFirst = true;
+static int dots = 0;
+
+int findMax(int l, int r)
+{
+  if (l > r) return l;
+  else return r;
+}
+
+void countDots(int *paper, int row, int col)
+{
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      if (paper[i*col + j] == 1) dots++;
+    }
+  }
+  isFirst = false;
+}
+
+void markTransparentPaper(int *paper, int x, int y, int col)
+{
+  paper[y*col + x]++;
+}
+
+int *foldTransparentPaperInRow(int *paper, int y, int *row, int col)
+{
+  int foldStart = y*col;
+  int foldEnd = y*col + col;
+  int startPos = (*row)*col - col;
+  int lastPos = (*row)*col - 1;
+  int distance = (lastPos - foldEnd + 1) * 2;
+  int mirror = startPos - distance;
+
+  for (int i = 0; i < y; i++) {
+    for (int j = 0; j < col; j++) {
+      if (paper[mirror] == 0) paper[mirror] = paper[startPos + j];
+      mirror++;
+    }
+    startPos -= col;
+  }
+
+  int *newPaper = calloc(foldStart, sizeof(int));
+  for (int i = 0; i < y; i++) {
+    for (int j = 0; j < col; j++) {
+      newPaper[i*col + j] = paper[i*col + j];
+    }
+  }
+
+  if (isFirst)
+    countDots(newPaper, y, col);
+
+  *row = y;
+  free(paper);
+  return newPaper;
+}
+
+int *foldTransparentPaperInCol(int *paper, int x, int row, int *col)
+{
+  int foldEnd = row*(*col) - x;
+  int startPos = *col - 1;
+  int lastPos = row*(*col) - 1;
+  int distance = (lastPos - foldEnd + 1) * 2;
+  int mirror = startPos - distance;
+
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < x; j++) {
+      if (paper[mirror] == 0) paper[mirror] = paper[startPos - j];
+      mirror++;
+    }
+    startPos += *col;
+    mirror = startPos - distance;
+  }
+
+  int *newPaper = calloc(foldEnd, sizeof(int));
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < x; j++) {
+      newPaper[i*x + j] = paper[i*(*col) + j];
+    }
+  }
+
+  if (isFirst)
+    countDots(newPaper, row, x);
+
+  *col = x;
+  free(paper);
+  return newPaper;
+}
+
+int main(void)
+{
+  FILE *input = fopen("day13.txt", "r");
+  if (!input) {
+    perror("FileIOError");
+    exit(EXIT_FAILURE);
+  }
+
+  int x, y;
+  int maxX = 0, maxY = 0;
+  while (fscanf(input, "%d,%d\n", &x, &y) == 2) {
+    maxX = findMax(maxX, x);
+    maxY = findMax(maxY, y);
+  }
+  rewind(input);
+
+  int col = maxX + 1, row = maxY + 1;
+  int transparentPaperSize = col * row;
+  int *transparentPaper = calloc(transparentPaperSize, sizeof(int));
+  while (fscanf(input, "%d,%d\n", &x, &y) == 2) {
+    markTransparentPaper(transparentPaper, x, y, col);
+  }
+
+  char *instruction = malloc(13 * sizeof(char));
+  int pos;
+  while (fscanf(input, "%[^=]=%d\n", instruction, &pos) == 2) {
+    if (strcmp(instruction, "fold along y") == 0) {
+      transparentPaper = foldTransparentPaperInRow(transparentPaper, pos, &row, col);
+    } else if (strcmp(instruction, "fold along x") == 0) {
+      transparentPaper = foldTransparentPaperInCol(transparentPaper, pos, row, &col);
+    }
+  }
+
+  printf("Number of dots: %d\n", dots);
+
+  free(instruction);
+  free(transparentPaper);
+  fclose(input);
+  exit(EXIT_SUCCESS);
+}
